e28d79af163b85f633445d163310681f30a355ea
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-08-15 11:31:26 -0400

    updated desktop formatting

--- src/components/DesktopHomepage.js
+++ src/components/DesktopHomepage.js
@@ -1,34 +1,40 @@
 import React from "react";
-import { Menu, Container } from "semantic-ui-react";
+import { Menu, Container, Grid } from "semantic-ui-react";
 
 function DesktopHomepage() {
   return (
-    <Menu borderless secondary pointing fixed="top">
-      <Container>
+    <Grid as={Container}>
+      <Grid.Column width={8}>
         <img
           src="cat-logo.png"
+          className="cat logo"
           height="30px"
           style={{
             marginRight: "14px",
-            top: "50%",
+            width: "auto",
+            marginTop: "20px",
           }}
           alt="/"
         />
         <h1 className="logo"> Ava Hajratwala </h1>
-        <Menu.Item as="a" href="./" active position="right">
-          About
-        </Menu.Item>
-        <Menu.Item as="a" href="./devblog " position="right">
-          Devblog
-        </Menu.Item>
-        <Menu.Item as="a" href="./project-hub" position="right">
-          Projects
-        </Menu.Item>
-        <Menu.Item as="a" href="./academics" position="right">
-          Academics
-        </Menu.Item>
-      </Container>
-    </Menu>
+      </Grid.Column>
+      <Grid.Column>
+        <Menu secondary>
+          <Menu.Item as="a" href="./" active position="right">
+            About
+          </Menu.Item>
+          <Menu.Item as="a" href="./devblog ">
+            Devblog
+          </Menu.Item>
+          <Menu.Item as="a" href="./project-hub">
+            Projects
+          </Menu.Item>
+          <Menu.Item as="a" href="./academics">
+            Academics
+          </Menu.Item>
+        </Menu>
+      </Grid.Column>
+    </Grid>
   );
 }
 
